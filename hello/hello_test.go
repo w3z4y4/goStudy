4058087208847d949eca8d8dface8adaf0dace4b
王泽洋 <876989946@qq.com>
2021-12-27 11:08:39 +0800

    测试重构

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,21 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+
 }
